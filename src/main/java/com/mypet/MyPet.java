1913a0659965ad7523db936cbe4173b7a4c0f478
Nguyễn Quang Vinh <nqvinh7603@gmail.com>
2023-09-15 21:30:53 +0700

    init project

---
+++ src/main/java/com/mypet/MyPet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mypet;
+
+/**
+ *
+ * @author Admin
+ */
+public class MyPet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
